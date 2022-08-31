5352bb2191b53d8c615343e450184d3652126a13
mpulatov.developer@gmail.com <mpulatov@fardo.lc>
2022-08-31 11:42:33 +0500

    added About / Header pages, made some changes to eslint

--- frontend_portfolio/src/container/About/About.jsx
+++ frontend_portfolio/src/container/About/About.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
+import { AppWrap } from "../../wrapper";
 import "./About.scss";
+import { urlFor, client } from "../../client";
 
-const About = () => <div>About</div>;
+// import {abouts} from "../../data/data"
 
-export default About;
+const About = () => {
+  const [abouts, setAboutes] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAboutes(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2 className="head-text">
+        I Know that <span>Good Apps</span>
+        <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 20 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AppWrap(About, "About");
